abd67875003cc94719f30abf68ec47348986afa1
Cheryl K <cherylkelson@icloud.com>
2019-01-29 20:23:02 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_characters.rb
+++ db/migrate/004_create_characters.rb
@@ -1,10 +1,14 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
 
   def change
-  create_table :characters do |t|
+
+create_table :characters do |t|
     t.string :name
     t.integer :show_id
     t.integer :actor_id
+
     end
+
   end
+
 end
