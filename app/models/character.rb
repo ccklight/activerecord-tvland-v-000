0b40ae9707408d85bb20924bdecf0c2a9a957ee6
Cheryl K <cherylkelson@icloud.com>
2019-01-29 19:34:10 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,19 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
+  def catchphrase
+    c = "Did I do that?" 
+    
+  '#{self.name} always says: #{self.catchphrase}"
+
+
+      def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+      end
+    end
+
+    
+
+  end 
+
 end
