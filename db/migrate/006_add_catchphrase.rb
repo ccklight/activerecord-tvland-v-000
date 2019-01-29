2bdb3510199811020244dd4238f8b4a8ea46d8ce
Cheryl K <cherylkelson@icloud.com>
2019-01-29 20:16:50 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase.rb
+++ db/migrate/006_add_catchphrase.rb
@@ -1,7 +1,12 @@
-class AddCatchphrase < ActiveRecord::Migration[4.2]
+class AddCatchphrasetoCharacter < ActiveRecord::Migration[4.2]
 
   def change
     add_column :characters, :catchphrase, :string
   end
 
+end
+class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :catchphrase, :string
+  end
 end
