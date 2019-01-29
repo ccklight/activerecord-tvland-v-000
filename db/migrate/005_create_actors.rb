5b60d0cc3ac96c391c7077db015237ab5d1ad1cc
Cheryl K <cherylkelson@icloud.com>
2019-01-29 19:24:38 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_actors.rb
+++ db/migrate/005_create_actors.rb
@@ -4,10 +4,10 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |t|
       t.string  :first_name
       t.string  :last_name
-    
+
       end
   end
 
-  def full_name 
+  def full_name
     '#{first_}
   end
