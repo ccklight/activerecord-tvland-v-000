0747324175835df1dcaca3035901c93db19be9f6
Cheryl K <cherylkelson@icloud.com>
2019-01-29 19:26:46 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,3 +4,14 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
 end
+
+def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+end
